c16db9c8b1bb37eb3c8754834661dc1d7073965a
Michael Bach Jensen <michael@ifad.dk>
2018-10-02 14:37:48 +0200

    Apply patch from sba: Fix multi-threading outputstream problem as described in http://forum.openscenegraph.org/viewtopic.php?t=12256

--- src/osg/Notify.cpp
+++ src/osg/Notify.cpp
@@ -21,102 +21,171 @@
 
 #include <ctype.h>
 
+#define OSG_NOTIFY_STREAM_PER_THREAD // enables or disables the workaround
+
+#ifdef OSG_NOTIFY_STREAM_PER_THREAD
+#include <OpenThreads/Thread>
+#include <OpenThreads/ScopedLock>
+#include <map>
+#endif // OSG_NOTIFY_STREAM_PER_THREAD
+
 #define OSG_INIT_SINGLETON_PROXY(ProxyName, Func) static struct ProxyName{ ProxyName() { Func; } } s_##ProxyName;
 
 namespace osg
 {
 
-class NullStreamBuffer : public std::streambuf
-{
-private:
-    std::streamsize xsputn(const std::streambuf::char_type * /*str*/, std::streamsize n)
-    {
-        return n;
-    }
-};
+	class NullStreamBuffer : public std::streambuf
+	{
+	private:
+		std::streamsize xsputn(const std::streambuf::char_type * /*str*/, std::streamsize n)
+		{
+			return n;
+		}
+	};
 
-struct NullStream : public std::ostream
-{
-public:
-    NullStream():
-        std::ostream(new NullStreamBuffer)
+	struct NullStream : public std::ostream
+	{
+	public:
+		NullStream() :
+			std::ostream(new NullStreamBuffer)
     { _buffer = dynamic_cast<NullStreamBuffer *>(rdbuf()); }
 
-    ~NullStream()
-    {
-        rdbuf(0);
-        delete _buffer;
-    }
+		~NullStream()
+		{
+			rdbuf(0);
+			delete _buffer;
+		}
 
-protected:
-    NullStreamBuffer* _buffer;
-};
+	protected:
+		NullStreamBuffer* _buffer;
+	};
 
-/** Stream buffer calling notify handler when buffer is synchronized (usually on std::endl).
- * Stream stores last notification severity to pass it to handler call.
- */
-struct NotifyStreamBuffer : public std::stringbuf
-{
-    NotifyStreamBuffer() : _severity(osg::NOTICE)
-    {
-    }
+	/** Stream buffer calling notify handler when buffer is synchronized (usually on std::endl).
+	* Stream stores last notification severity to pass it to handler call.
+	*/
+	struct NotifyStreamBuffer : public std::stringbuf
+	{
+		NotifyStreamBuffer() : _severity(osg::NOTICE)
+		{
+		}
 
-    void setNotifyHandler(osg::NotifyHandler *handler) { _handler = handler; }
-    osg::NotifyHandler *getNotifyHandler() const { return _handler.get(); }
+		void setNotifyHandler(osg::NotifyHandler *handler) { _handler = handler; }
+		osg::NotifyHandler *getNotifyHandler() const { return _handler.get(); }
 
-    /** Sets severity for next call of notify handler */
-    void setCurrentSeverity(osg::NotifySeverity severity)
-    {
-        if (_severity != severity)
-        {
-            sync();
-            _severity = severity;
-        }
-    }
+		/** Sets severity for next call of notify handler */
+		void setCurrentSeverity(osg::NotifySeverity severity)
+		{
+			if (_severity != severity)
+			{
+				sync();
+				_severity = severity;
+			}
+		}
 
-    osg::NotifySeverity getCurrentSeverity() const { return _severity; }
+		osg::NotifySeverity getCurrentSeverity() const { return _severity; }
 
-private:
+	private:
 
-    int sync()
-    {
-        sputc(0); // string termination
-        if (_handler.valid())
-            _handler->notify(_severity, pbase());
-        pubseekpos(0, std::ios_base::out); // or str(std::string())
-        return 0;
-    }
+		int sync()
+		{
+			sputc(0); // string termination
+			if (_handler.valid())
+				_handler->notify(_severity, pbase());
+			pubseekpos(0, std::ios_base::out); // or str(std::string())
+			return 0;
+		}
 
-    osg::ref_ptr<osg::NotifyHandler> _handler;
-    osg::NotifySeverity _severity;
-};
+		osg::ref_ptr<osg::NotifyHandler> _handler;
+		osg::NotifySeverity _severity;
+	};
 
-struct NotifyStream : public std::ostream
-{
-public:
-    NotifyStream():
-        std::ostream(new NotifyStreamBuffer)
+	struct NotifyStream : public std::ostream
+	{
+	public:
+		NotifyStream() :
+			std::ostream(new NotifyStreamBuffer)
     { _buffer = dynamic_cast<NotifyStreamBuffer *>(rdbuf()); }
 
-    void setCurrentSeverity(osg::NotifySeverity severity)
-    {
-        _buffer->setCurrentSeverity(severity);
-    }
+		void setCurrentSeverity(osg::NotifySeverity severity)
+		{
+			_buffer->setCurrentSeverity(severity);
+		}
 
-    osg::NotifySeverity getCurrentSeverity() const
-    {
-        return _buffer->getCurrentSeverity();
-    }
+		osg::NotifySeverity getCurrentSeverity() const
+		{
+			return _buffer->getCurrentSeverity();
+		}
 
-    ~NotifyStream()
-    {
-        rdbuf(0);
-        delete _buffer;
-    }
+		~NotifyStream()
+		{
+			rdbuf(0);
+			delete _buffer;
+		}
 
-protected:
-    NotifyStreamBuffer* _buffer;
-};
+	protected:
+		NotifyStreamBuffer* _buffer;
+	};
+
+#ifdef OSG_NOTIFY_STREAM_PER_THREAD
+	class StreamPerThreadStorage
+	{
+	private:
+		typedef std::map< OpenThreads::Thread*, std::ostream* > StreamPerThreadMap;
+
+	public:
+		StreamPerThreadStorage()
+			: _mutex(OpenThreads::Mutex::MUTEX_NORMAL)
+		{
+		}
+
+		~StreamPerThreadStorage()
+		{
+			OpenThreads::ScopedLock<OpenThreads::Mutex> locker(_mutex);
+
+			for (StreamPerThreadMap::iterator it = _streams.begin(); it != _streams.end(); ++it)
+			{
+				if (it->second)
+					delete it->second;
+			}
+
+			_streams.clear();
+		}
+
+		bool hasLocalStream() const
+		{
+			return (getLocalStream() != 0);
+		}
+
+		std::ostream* getLocalStream() const
+		{
+			OpenThreads::Thread *thread = OpenThreads::Thread::CurrentThread();
+			OpenThreads::ScopedLock<OpenThreads::Mutex> locker(_mutex);
+			StreamPerThreadMap::const_iterator it = _streams.find(thread);
+			return (it != _streams.end()) ? it->second : 0;
+		}
+
+		void setLocalStream(std::ostream *stream)
+		{
+			OpenThreads::Thread *thread = OpenThreads::Thread::CurrentThread();
+			OpenThreads::ScopedLock<OpenThreads::Mutex> locker(_mutex);
+			StreamPerThreadMap::const_iterator it = _streams.find(thread);
+			std::ostream *oldStream = (it != _streams.end()) ? it->second : 0;
+
+			_streams[thread] = stream;
+
+			if (oldStream)
+				delete oldStream;
+		}
+
+	private:
+		// Disable copying
+		StreamPerThreadStorage(const StreamPerThreadStorage &other);
+		StreamPerThreadStorage& operator=(const StreamPerThreadStorage &other);
+
+		mutable OpenThreads::Mutex _mutex;
+		StreamPerThreadMap _streams;
+	};
+#endif // OSG_NOTIFY_STREAM_PER_THREAD
 
 }
 
@@ -126,61 +195,73 @@ static osg::ApplicationUsageProxy Notify_e0(osg::ApplicationUsage::ENVIRONMENTAL
 
 struct NotifySingleton
 {
-    NotifySingleton()
-    {
-        // _notifyLevel
-        // =============
+	NotifySingleton()
+	{
+		// _notifyLevel
+		// =============
 
-        _notifyLevel = osg::NOTICE; // Default value
+		_notifyLevel = osg::NOTICE; // Default value
 
-        char* OSGNOTIFYLEVEL=getenv("OSG_NOTIFY_LEVEL");
-        if (!OSGNOTIFYLEVEL) OSGNOTIFYLEVEL=getenv("OSGNOTIFYLEVEL");
-        if(OSGNOTIFYLEVEL)
-        {
+		char* OSGNOTIFYLEVEL = getenv("OSG_NOTIFY_LEVEL");
+		if (!OSGNOTIFYLEVEL) OSGNOTIFYLEVEL = getenv("OSGNOTIFYLEVEL");
+		if (OSGNOTIFYLEVEL)
+		{
 
-            std::string stringOSGNOTIFYLEVEL(OSGNOTIFYLEVEL);
+			std::string stringOSGNOTIFYLEVEL(OSGNOTIFYLEVEL);
 
-            // Convert to upper case
-            for(std::string::iterator i=stringOSGNOTIFYLEVEL.begin();
-                i!=stringOSGNOTIFYLEVEL.end();
-                ++i)
-            {
-                *i=toupper(*i);
-            }
+			// Convert to upper case
+			for (std::string::iterator i = stringOSGNOTIFYLEVEL.begin();
+				i != stringOSGNOTIFYLEVEL.end();
+				++i)
+			{
+				*i = toupper(*i);
+			}
 
-            if(stringOSGNOTIFYLEVEL.find("ALWAYS")!=std::string::npos)          _notifyLevel=osg::ALWAYS;
-            else if(stringOSGNOTIFYLEVEL.find("FATAL")!=std::string::npos)      _notifyLevel=osg::FATAL;
-            else if(stringOSGNOTIFYLEVEL.find("WARN")!=std::string::npos)       _notifyLevel=osg::WARN;
-            else if(stringOSGNOTIFYLEVEL.find("NOTICE")!=std::string::npos)     _notifyLevel=osg::NOTICE;
-            else if(stringOSGNOTIFYLEVEL.find("DEBUG_INFO")!=std::string::npos) _notifyLevel=osg::DEBUG_INFO;
-            else if(stringOSGNOTIFYLEVEL.find("DEBUG_FP")!=std::string::npos)   _notifyLevel=osg::DEBUG_FP;
-            else if(stringOSGNOTIFYLEVEL.find("DEBUG")!=std::string::npos)      _notifyLevel=osg::DEBUG_INFO;
-            else if(stringOSGNOTIFYLEVEL.find("INFO")!=std::string::npos)       _notifyLevel=osg::INFO;
-            else std::cout << "Warning: invalid OSG_NOTIFY_LEVEL set ("<<stringOSGNOTIFYLEVEL<<")"<<std::endl;
+			if (stringOSGNOTIFYLEVEL.find("ALWAYS") != std::string::npos)          _notifyLevel = osg::ALWAYS;
+			else if (stringOSGNOTIFYLEVEL.find("FATAL") != std::string::npos)      _notifyLevel = osg::FATAL;
+			else if (stringOSGNOTIFYLEVEL.find("WARN") != std::string::npos)       _notifyLevel = osg::WARN;
+			else if (stringOSGNOTIFYLEVEL.find("NOTICE") != std::string::npos)     _notifyLevel = osg::NOTICE;
+			else if (stringOSGNOTIFYLEVEL.find("DEBUG_INFO") != std::string::npos) _notifyLevel = osg::DEBUG_INFO;
+			else if (stringOSGNOTIFYLEVEL.find("DEBUG_FP") != std::string::npos)   _notifyLevel = osg::DEBUG_FP;
+			else if (stringOSGNOTIFYLEVEL.find("DEBUG") != std::string::npos)      _notifyLevel = osg::DEBUG_INFO;
+			else if (stringOSGNOTIFYLEVEL.find("INFO") != std::string::npos)       _notifyLevel = osg::INFO;
+			else std::cout << "Warning: invalid OSG_NOTIFY_LEVEL set (" << stringOSGNOTIFYLEVEL << ")" << std::endl;
 
-        }
+		}
 
-        // Setup standard notify handler
-        osg::NotifyStreamBuffer *buffer = dynamic_cast<osg::NotifyStreamBuffer *>(_notifyStream.rdbuf());
-        if (buffer && !buffer->getNotifyHandler())
-            buffer->setNotifyHandler(new StandardNotifyHandler);
-    }
+#ifndef OSG_NOTIFY_STREAM_PER_THREAD
+		// Setup standard notify handler
+		osg::NotifyStreamBuffer *buffer = dynamic_cast<osg::NotifyStreamBuffer *>(_notifyStream.rdbuf());
+		if (buffer && !buffer->getNotifyHandler())
+			buffer->setNotifyHandler(new StandardNotifyHandler);
+#else
+		// Setup standard notify handler
+		if (!_notifyHandler.valid())
+			_notifyHandler = new StandardNotifyHandler();
+#endif // OSG_NOTIFY_STREAM_PER_THREAD
+	}
 
-    osg::NotifySeverity _notifyLevel;
-    osg::NullStream     _nullStream;
-    osg::NotifyStream   _notifyStream;
+	osg::NotifySeverity _notifyLevel;
+#ifndef OSG_NOTIFY_STREAM_PER_THREAD
+	osg::NullStream     _nullStream;
+	osg::NotifyStream   _notifyStream;
+#else
+	osg::ref_ptr<osg::NotifyHandler> _notifyHandler;
+	osg::StreamPerThreadStorage _nullStreams;
+	osg::StreamPerThreadStorage _notifyStreams;
+#endif // OSG_NOTIFY_STREAM_PER_THREAD
 };
 
 static NotifySingleton& getNotifySingleton()
 {
-    static NotifySingleton s_NotifySingleton;
-    return s_NotifySingleton;
+	static NotifySingleton s_NotifySingleton;
+	return s_NotifySingleton;
 }
 
 bool osg::initNotifyLevel()
 {
-    getNotifySingleton();
-    return true;
+	getNotifySingleton();
+	return true;
 }
 
 // Use a proxy to force the initialization of the the NotifySingleton during static initialization
@@ -188,62 +269,105 @@ OSG_INIT_SINGLETON_PROXY(NotifySingletonProxy, osg::initNotifyLevel())
 
 void osg::setNotifyLevel(osg::NotifySeverity severity)
 {
-    getNotifySingleton()._notifyLevel = severity;
+	getNotifySingleton()._notifyLevel = severity;
 }
 
 osg::NotifySeverity osg::getNotifyLevel()
 {
-    return getNotifySingleton()._notifyLevel;
+	return getNotifySingleton()._notifyLevel;
 }
 
 void osg::setNotifyHandler(osg::NotifyHandler *handler)
 {
-    osg::NotifyStreamBuffer *buffer = static_cast<osg::NotifyStreamBuffer*>(getNotifySingleton()._notifyStream.rdbuf());
-    if (buffer) buffer->setNotifyHandler(handler);
+#ifndef OSG_NOTIFY_STREAM_PER_THREAD
+	osg::NotifyStreamBuffer *buffer = static_cast<osg::NotifyStreamBuffer*>(getNotifySingleton()._notifyStream.rdbuf());
+	if (buffer) buffer->setNotifyHandler(handler);
+#else
+	getNotifySingleton()._notifyHandler = handler;
+#endif // OSG_NOTIFY_STREAM_PER_THREAD
 }
 
 osg::NotifyHandler* osg::getNotifyHandler()
 {
-    osg::NotifyStreamBuffer *buffer = static_cast<osg::NotifyStreamBuffer *>(getNotifySingleton()._notifyStream.rdbuf());
-    return buffer ? buffer->getNotifyHandler() : 0;
+#ifndef OSG_NOTIFY_STREAM_PER_THREAD
+	osg::NotifyStreamBuffer *buffer = static_cast<osg::NotifyStreamBuffer *>(getNotifySingleton()._notifyStream.rdbuf());
+	return buffer ? buffer->getNotifyHandler() : 0;
+#else
+	return getNotifySingleton()._notifyHandler.get();
+#endif // OSG_NOTIFY_STREAM_PER_THREAD
 }
 
 
 #ifndef OSG_NOTIFY_DISABLED
-bool osg::isNotifyEnabled( osg::NotifySeverity severity )
+bool osg::isNotifyEnabled(osg::NotifySeverity severity)
 {
-    return severity<=getNotifySingleton()._notifyLevel;
+	return severity <= getNotifySingleton()._notifyLevel;
 }
 #endif
 
+#ifndef OSG_NOTIFY_STREAM_PER_THREAD
 std::ostream& osg::notify(const osg::NotifySeverity severity)
 {
-    if (osg::isNotifyEnabled(severity))
-    {
-        getNotifySingleton()._notifyStream.setCurrentSeverity(severity);
-        return getNotifySingleton()._notifyStream;
-    }
-    return getNotifySingleton()._nullStream;
+	if (osg::isNotifyEnabled(severity))
+	{
+		getNotifySingleton()._notifyStream.setCurrentSeverity(severity);
+		return getNotifySingleton()._notifyStream;
+	}
+	return getNotifySingleton()._nullStream;
 }
+#else
+std::ostream& osg::notify(const osg::NotifySeverity severity)
+{
+	if (osg::isNotifyEnabled(severity))
+	{
+		osg::NotifyStream *stream = dynamic_cast<osg::NotifyStream*>(getNotifySingleton()._notifyStreams.getLocalStream());
+		if (!stream)
+		{
+			stream = new osg::NotifyStream();
+			getNotifySingleton()._notifyStreams.setLocalStream(stream);
+		}
+
+		osg::NotifyStreamBuffer *buffer = dynamic_cast<osg::NotifyStreamBuffer*>(stream->rdbuf());
+		if (buffer)
+		{
+			buffer->setNotifyHandler(getNotifySingleton()._notifyHandler.get());
+			buffer->setCurrentSeverity(severity);
+		}
+
+		return *stream;
+	}
+	else
+	{
+		osg::NullStream *stream = dynamic_cast<osg::NullStream*>(getNotifySingleton()._nullStreams.getLocalStream());
+		if (!stream)
+		{
+			stream = new osg::NullStream();
+			getNotifySingleton()._nullStreams.setLocalStream(stream);
+		}
+
+		return *stream;
+	}
+}
+#endif // OSG_NOTIFY_STREAM_PER_THREAD
 
 void osg::StandardNotifyHandler::notify(osg::NotifySeverity severity, const char *message)
 {
-    if (severity <= osg::WARN)
-        fputs(message, stderr);
-    else
-        fputs(message, stdout);
+	if (severity <= osg::WARN)
+		fputs(message, stderr);
+	else
+		fputs(message, stdout);
 }
 
 #if defined(WIN32) && !defined(__CYGWIN__)
 
 #ifndef WIN32_LEAN_AND_MEAN
-    #define WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
 #endif
 #include <windows.h>
 
 void osg::WinDebugNotifyHandler::notify(osg::NotifySeverity severity, const char *message)
 {
-    OutputDebugStringA(message);
+	OutputDebugStringA(message);
 }
 
 #endif
