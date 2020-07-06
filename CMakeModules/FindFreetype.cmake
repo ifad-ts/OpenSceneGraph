e7fe11cde2f1db38dae3a467148e362a53050741
Michael Bach Jensen <michael@ifad.dk>
2020-07-06 12:07:17 +0200

    New freetype lib included in osgVisual v11 was not found.

--- CMakeModules/FindFreetype.cmake
+++ CMakeModules/FindFreetype.cmake
@@ -116,6 +116,7 @@ if(NOT FREETYPE_LIBRARY)
       freetype
       libfreetype
       freetype219
+      freetype271
     ${FREETYPE_FIND_ARGS}
     PATH_SUFFIXES
       lib
@@ -125,6 +126,7 @@ if(NOT FREETYPE_LIBRARY)
       freetyped
       libfreetyped
       freetype219d
+      freetype271d
     ${FREETYPE_FIND_ARGS}
     PATH_SUFFIXES
       lib
